0feb1da1d314cfc73477891d60e86ad79c2717b3
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-17 20:19:00 +0300

    finalendlab7

--- server.js
+++ server.js
@@ -1,46 +1,57 @@
 'use strict'
 
 const { request, response } = require('express');
-const express =require('express');
+const express = require('express');
 require('dotenv').config();
-const wetherdata =require('./data/wether.json');
-const server= express();
+const wetherdata = require('./data/wether.json');
+const server = express();
 const PORT = process.env.PORT;
 const cors = require('cors')
 server.use(cors());
 
-server.listen( PORT,()=>{
-console.log(`listning on port ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`listning on port ${PORT}`)
 })
+class Forcast {
+    constructor(item) {
+        this.data = item.datetime
+        this.description = item.weather.description
+    }
+
+}
 //localhost:3001/test
 server.get('/test', (request, response) => {
     response.send('your server is working')
 
 })
 //localhost:3001/
-server.get('/',(request,response)=>{
+server.get('/', (request, response) => {
     response.send('home route')
 })
 
 
-//localhost:${process.env.REACT_APP_LOCATIONIQ_KEY}/whether?lat=0&lon=0&searchQuery='A'
- server.get('/whether', ( request, response) => {
-    let lat = request.query.lat
-    let lon = request.query.lon
-    let city_name = request.query.city_name
-    let citywhther = wetherdata.find ( async value => {
-          if  ( await Number(value.lat) === Number(lat) && Number(value.lon) == Number(lon) && value.city_name == city_name) {
-           return citywhther
-        }
-          
-          response.send([citywhther.data[0].weather,citywhther.data[0].valid_date] )
 
+
+server.get('/whethercity', (request, response) => {
+    // let lat = request.query.lat
+    // let lon = request.query.lon
+    let city_name = request.query.city_name
+    let whether = wetherdata.find(value => {
+        if (value.city_name.toLowerCase() == city_name.toLowerCase()) {return value }
     })
+
+    // let whether =wetherdata.find(e=>e.city_name.toLowerCase()==city_name.toLowerCase())
+
+    let Forcastarray = whether.data.map((item) => {
+
+        return new Forcast(item)
+    })
+    response.send(Forcastarray)
 
 
 
 })
-server.get('*',(req,res)=>{
+server.get('*', (req, res) => {
     res.status(500).send('not found')
 })
 
