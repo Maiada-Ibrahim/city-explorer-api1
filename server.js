5e9a3ccfd497f50c7f7089621c458a723931656a
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-17 20:38:01 +0300

    firstlab-08

--- server.js
+++ server.js
@@ -32,25 +32,54 @@ server.get('/', (request, response) => {
 
 
 
-server.get('/whethercity', (request, response) => {
-    // let lat = request.query.lat
-    // let lon = request.query.lon
-    let city_name = request.query.city_name
-    let whether = wetherdata.find(value => {
-        if (value.city_name.toLowerCase() == city_name.toLowerCase()) {return value }
-    })
+// server.get('/whethercity', (request, response) => {
+//     // let lat = request.query.lat
+//     // let lon = request.query.lon
+//     let city_name = request.query.city_name
+//     let whether = wetherdata.find(value => {
+//         if (value.city_name.toLowerCase() == city_name.toLowerCase()) {return value }
+//     })
 
-    // let whether =wetherdata.find(e=>e.city_name.toLowerCase()==city_name.toLowerCase())
+//     // let whether =wetherdata.find(e=>e.city_name.toLowerCase()==city_name.toLowerCase())
 
-    let Forcastarray = whether.data.map((item) => {
+//     let Forcastarray = whether.data.map((item) => {
 
-        return new Forcast(item)
-    })
-    response.send(Forcastarray)
+//         return new Forcast(item)
+//     })
+//     response.send(Forcastarray)
 
 
 
-})
+// })
+
+server.get('/wether', getwhether)
+function getwhether(req, res) {
+    let lat = request.query.lat
+    let lon = request.query.lon
+    let url = `https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely`
+    
+    console.log('before axios');
+    try {
+        axios.get(url).then((photoResults) => {
+            console.log('inside axios');
+
+            // console.log(photoResults.data)
+            let photoArray = photoResults.data.results.map(photo => {
+                return new Photo(photo)
+            })
+            res.send(photoArray)
+        })
+    }
+    catch (error) {
+        console.log('error from axios', error)
+        res.send(error)
+    }
+
+    console.log('after axios');
+
+}
+
+
 server.get('*', (req, res) => {
     res.status(500).send('not found')
 })
