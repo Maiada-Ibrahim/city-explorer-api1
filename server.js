3f45be7bfe2f49b44318290c883a7bc89f3f8543
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-08-18 14:30:38 +0300

    finishweather

--- server.js
+++ server.js
@@ -17,38 +17,42 @@ class Forcast {
     constructor(item) {
         this.data =item.valid_date
         this.description = item.weather.description
+        this.temp=item.temp
     }
 
 }
-
-
-
-
-// server.get('/whethercity', (request, response) => {
-//     // let lat = request.query.lat
-//     // let lon = request.query.lon
-//     let city_name = request.query.city_name
-//     let whether = wetherdata.find(value => {
-//         if (value.city_name.toLowerCase() == city_name.toLowerCase()) {return value }
-//     })
-
-//     // let whether =wetherdata.find(e=>e.city_name.toLowerCase()==city_name.toLowerCase())
-
-//     let Forcastarray = whether.data.map((item) => {
-
-//         return new Forcast(item)
-//     })
-//     response.send(Forcastarray)
-
-
-
-// })
 server.get('/', homeHandler);
 // Function Handlers
 function homeHandler (req, res) {
     res.send('all good')}
 
 server.get('/whethertoday', getwhether)
+function getwhether (req, res) {
+    let city = req.query.city
+    console.log(city)
+    let url =`https://api.weatherbit.io/v2.0/forecast/daily?city=${city},&key=${process.env.WEATHER_API_KEY}`
+    console.log('before axios wheather');
+    try  {
+        axios.get(url).then((whetherResults) => {
+            console.log('inside axios wheather');
+            // console.log(whetherResults.data)
+            let wetherArray = whetherResults.data.data.map(item => {
+                return new Forcast(item)
+            })
+            res.send(wetherArray)
+            console.log(wetherArray)
+        })
+    }
+    catch (error) {
+        console.log('error from axios wheather', error)
+        res.send(error)
+    }
+
+    console.log('after axios wheather');
+
+}
+// http://localhost:3001/weather?city=Amman
+server.get('/weather', getwhether)
 function getwhether (req, res) {
     // let lat = req.query.lat
     // console.log(lat)
@@ -65,7 +69,7 @@ function getwhether (req, res) {
         axios.get(url).then((whetherResults) => {
             console.log('inside axios');
 
-            // console.log(whetherResults.data)
+            console.log(whetherResults.data.data)
             let wetherArray = whetherResults.data.data.map(item => {
                 return new Forcast(item)
             })
@@ -82,10 +86,8 @@ function getwhether (req, res) {
 
 }
 
-
 server.get('*', (req, res) => {
     res.status(500).send('not found')
 })
 
 
-// http://localhost:3001/whethertoday?city=Amman
